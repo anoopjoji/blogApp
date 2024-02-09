fabf9fae08a7d62c7e20e84435aef72ef0b0d25d
anoopjoji <anoopjoji@gmail.com>
2024-02-09 11:49:02 +0530

    Secured

--- controller/userRouter.js
+++ controller/userRouter.js
@@ -2,13 +2,30 @@ const express = require("express")
 const usersModels = require("../models/usersModels")
 const router = express.Router()
 
-router.post("/signup", async (req, res) => {
-    let data = req.body
-    let user = new usersModels(data)
-    let result = await user.save()
-    res.json({
-        status: "success"
-    })
+const bcrypt = require("bcryptjs")
+
+hashPaswordGenerator =async(pass)=>{
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+
+router.post("/signup", async(req,res)=>{
+    let {data} = {"data":req.body}
+      let password =data.password
+        hashPaswordGenerator(password).then(
+            (hashedPassword)=>{
+                console.log(hashedPassword)
+                data.password=hashedPassword
+                console.log(data)
+                
+                 let user =new usersModels(data)
+                let result =user.save()
+                
+                res.json({status:"success"})
+            }
+        )
+
+   
 })
 
-module.exports=router
+module.exports =router
